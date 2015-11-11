a066b43d291a9e04f68d8981a022543c2b82f9b4
Nikita Abraham <nikitaa1995@gmail.com>
2015-11-11 00:09:35 -0500

    Floating Score values shown to Player

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -18,6 +18,11 @@ public class Prospector : MonoBehaviour {
 	static public int SCORE_FROM_PREV_ROUND = 0;
 	static public int HIGH_SCORE = 0;
 
+	public Vector3 fsPosMid = new Vector3 (0.5f, 0.90f, 0);
+	public Vector3 fsPosRun = new Vector3 (0.5f, 0.75f, 0);
+	public Vector3 fsPosMid2 = new Vector3 (0.5f, 0.5f, 0);
+	public Vector3 fsPosEnd = new Vector3 (1.0f, 0.65f, 0);
+
 	public Deck					deck;
 	public TextAsset			deckXML;
 
@@ -38,6 +43,7 @@ public class Prospector : MonoBehaviour {
 	public int chain = 0; //number of cards in this run
 	public int scoreRun = 0;
 	public int score = 0;
+	public FloatingScore fsRun;
 
 	void Awake(){
 		S = this;
@@ -52,6 +58,7 @@ public class Prospector : MonoBehaviour {
 	}
 
 	void Start() {
+		Scoreboard.S.score = score;
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle(ref deck.cards); // this shuffles the deck
@@ -307,6 +314,7 @@ public class Prospector : MonoBehaviour {
 
 	//ScoreManager handles all of the scoring 
 	void ScoreManager(ScoreEvent sEvt) {
+		List<Vector3> fsPts;
 		switch (sEvt) {
 			// Same things need to happen whether it is a draw, a win or a loss
 		case ScoreEvent.draw: // Drawing a card
@@ -315,10 +323,41 @@ public class Prospector : MonoBehaviour {
 			chain = 0; // resets the score chain
 			score += scoreRun; // add scoreRun to total score
 			scoreRun = 0; // reset scoreRun
+			//Add fsRun to the _Scoreboard score
+			if (fsRun != null) {
+				//create points for the Bezier curve
+				fsPts = new List<Vector3>();
+				fsPts.Add (fsPosRun);
+				fsPts.Add (fsPosMid2);
+				fsPts.Add (fsPosEnd);
+				fsRun.reportFinishTo = Scoreboard.S.gameObject;
+				fsRun.Init(fsPts, 0, 1);
+				// Also adjust the fontSize
+				fsRun.fontSizes = new List<float>(new float[] {28,36,4});
+				fsRun = null; // Clear fsRun so it is created again
+			}
 			break;
 		case ScoreEvent.mine: // Remove a mine card
 			chain++; // increases the score chain
 			scoreRun += chain; // add score for this card to run
+			//create a FloatingScore for this score
+			FloatingScore fs;
+			//move it from the mousePosition to fsPosRun
+			Vector3 p0 = Input.mousePosition;
+			p0.x /= Screen.width;
+			p0.y /= Screen.height;
+			fsPts = new List<Vector3>();
+			fsPts.Add (p0);
+			fsPts.Add (fsPosMid);
+			fsPts.Add (fsPosRun);
+			fs = Scoreboard.S.CreateFloatingScore(chain,fsPts);
+			fs.fontSizes = new List<float>(new float[] {4,50,28});
+			if (fsRun == null) {
+				fsRun = fs;
+				fsRun.reportFinishTo = null;
+			} else {
+				fs.reportFinishTo = fsRun.gameObject;
+			}
 			break;
 		}
 		
---
+++ Assets/__Scripts/Scoreboard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+	//Scoreboard class manages showing the score to the player
+	public class Scoreboard : MonoBehaviour {
+		public static Scoreboard S; // singleton for Scoreboard
+		
+		public GameObject prefabFloatingScore;
+		
+		public bool   ____________________;
+		[SerializeField]
+		private int _score = 0;
+		public string _scoreString;
+		
+		//the score property also sets the scoreString
+		public int score {
+			get {
+				return(_score);
+			}
+			set {
+				_score = value;
+				_scoreString = Utils.AddCommasToNumber (_score);
+			}
+		}
+		
+		//the scoreString property also sets the GUIText.text
+		public string scoreString {
+			get {
+				return(_scoreString);
+			}
+			set {
+				_scoreString = value;
+				GetComponent<GUIText>().text = _scoreString;
+			}
+		}
+		
+		void Awake() {
+			S = this;
+		}
+		
+		//when called by SendMessage, this adds the fs.score to this.score
+		public void FSCallback(FloatingScore fs) {
+			score += fs.score;
+		}
+		
+		//this will Instantiate a new FloatingScore GameObject and initialize it.
+		//it also returns a pointer to the FloatingScore created so that the
+		//calling function can do more with it (like set fontSizes, etc.)
+		public FloatingScore CreateFloatingScore (int amt, List<Vector3> pts) {
+			GameObject go = Instantiate (prefabFloatingScore) as GameObject;
+			FloatingScore fs = go.GetComponent<FloatingScore> ();
+			fs.score = amt;
+			fs.reportFinishTo = this.gameObject; // Set fs to call back to this
+			fs.Init (pts);
+			return(fs);
+		}
+	}
